0e09c4fa84b959f03694e0633bc0ce2afc2cb1f9
Stanislav <ilchenko_stas@mail.ru>
2021-02-01 20:08:30 +0300

    Lesson 2

--- task3.rb
+++ task3.rb
@@ -3,11 +3,3 @@ while (fibbs.last + fibbs[fibbs.count - 2]) < 100
   fibbs << fibbs.last + fibbs[fibbs.count - 2]
 end
 puts fibbs
-
-# fibbonachi = [0, 1]
-
-# while (fibbonachi.last + fibbonachi[fibbonachi.count - 2]) < 100
-#   fibbonachi << fibbonachi.last + fibbonachi[fibbonachi.count - 2]
-# end
-
-# puts fibbonachi
